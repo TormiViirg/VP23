3f697761212faf31218d801ab9a0d0ca9696035c
Tormi Viirg <TormiViirg@users.noreply.github.com>
2023-09-28 13:32:34 +0300

    /home/toriirg/public_html/VP23

--- www.js
+++ www.js
@@ -1,9 +1,44 @@
 const http = require("http");
-
+const path = require("path");
+const url = require("url");
+const fs = require("fs")
+const pageHead = '<!DOCTYPE html>\n<html>\n<head>\n\t<meta charset="utf-8">\n\t<title>Tormi Viirg, veebiprogrammeerimine 2023</title>\n</head>\n<body>':
+const pageFoot =
 http.createServer(function(req, res){
-	//määrame tagastavate andmete päise, t on veebileht
-	res.writeHead(200, {"Content-Type": "text/html"});
-	res.write('<!DOCTYPE html><html><head><meta charset="utf-8"><title>Tormi Viirg, veebiprogrammeerimine 2023</title></head><body>')
-	res.write('<h1>Tormi Viirg </h1><p>See leht on loodud <a href="https://www.tlu.ee" target="_blank">TLÜ Digitehnoloogiate</a> instituudis õppetöö raames!</p><hr><p> Lenini päris laip on maetud Balti jaama 5-nda raja alla!!!<p><p> Parim viis karsklane olla on liiter viina peole kaasa võtta, see kõigi ees ühe sõõmuga alla lasta ja järgmistel pidudel teistelt kerjata, kuna nii ei paku keegi sulle seda enam.<p><p> Now, are there any other horrors you wish to try and insert into my system, or is your electronic butchery done?<p><hr><p>Kursus, mille raames see travestia loodi  on veebiprogrammeerimine.</body></html>')
-	return res.end();
+	console.log(url.parse(req.url, true));
+	let currentURL = url.parse(req.url, true);
+	if(currentURL.pathname === "/"){
+		//määrame tagastavate andmete päise, t on veebileht
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.write(pageHead);
+		res.write(pageBanner);
+		res.write(pageBody);
+		res.write('<p><a href="addName">Lisame nime!</p>');
+		res.write(pageFoot);
+		res.write('\n\t<img src="public/banner/banner.png" alt =">"Lehe bänner">\n');
+		res.write('<h1>Tormi Viirg </h1><p>See leht on loodud <a href="https://www.tlu.ee" target="_blank">TLÜ Digitehnoloogiate</a> instituudis õppetöö raames!</p><hr><p> Ärge siia kirjutatut liiga hoolikalt vaadake, kunagi ei ea mida leida võib. <p> Now, are there any other horrors you wish to try and insert into my system, or is your electronic butchery done?<p><hr><p>Kursus, mille raames see travestia loodi  on veebiprogrammeerimine.</body></html>')
+		return res.end();
+	} 
+	else if (currentURL.pathname === "/addName"){
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.write(pageHead);
+		res.write(pageBanner);
+		res.write(pageBody);
+		res.write('<h2>Palun lisa siia oma nimi</h2>');
+		res.write(pageFoot);
+		return res.end();
+	}
+	else if (currentURL.pathname === "/banner.png"){
+		console.log("Tahan pilti!!!");
+		let filePath = path.join(__dirname, "public", "banner/banner.png");
+		fs.readFile(filePath, (err, data)=>{
+			if(err){
+				throw err;
+			}
+			else {
+				res.writeHead(200,{"Content-Type": "image/png"});
+				res.end(data);
+			}
+		});
+	}
 }).listen(5207)
